5a14815708f68426d385bb07fb9cffe557e266c3
jessesbyers <jesse.s.byers@gmail.com>
2020-01-20 09:43:10 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,29 @@
 class Song < ApplicationRecord
     validates :title, presence: true
+    validate :future_release_year
+    validate :release_year_validation
+    # validate :same_song_twice
 
 
-    # is invalid without release year when released is true (FAILED - 7)
-    # is valid without release year when released is false
-    # is invalid when the release year is in the future (FAILED - 8)
-    # is invalid if an artist tries to release the same song twice in a year (FAILED - 9)
+    def release_year_validation
+        if released == true && !release_year.present?
+            errors.add(:release_year, "release year is required")
+        end
+    end
 
+    def future_release_year
+        if release_year.present? && release_year > Time.now.year
+            errors.add(:release_year, "release year may not be in the future")
+        end
+    end
+
+# is invalid if an artist tries to release the same song twice in a year (FAILED - 9)
+# not yet working
+    # def same_song_twice
+    #     Song.all.each do |song|
+    #         if song.release_year == :release_year && song.title == :title
+    #             errors.add(:released, "can't release same song twice in a year")
+    #         end
+    #     end
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'songs#index'
   resources :songs
+  # , only: [:show, :new, :create, :edit, :update, :destroy]
 end
