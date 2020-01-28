567d754e382070f4b7474a5a50fc97ce56ffb95c
zanibruno <corporate.zanin@gmail.com>
2020-01-28 15:34:36 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,29 @@
+class Song < ApplicationRecord
+
+    validates :title, presence: true 
+    validates :title, uniqueness: {scope: %i[release_year artist_name], message: "cannot repeat on the same year"}
+    validates :artist_name, presence: true 
+    validates :genre, presence: true 
+
+    validates :released, inclusion: {in: [true, false]}
+    validates :artist_name, presence: true
+
+    with_options if: :released? do |song|
+        song.validates :release_year, presence: true 
+        song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year
+    }
+end 
+
+def released?
+    released 
+end
+
+end
+
+#Song Title - Whateber
+#Song Artist - WHoever 
+#Released? - False
+#Release year - 1992
+
+
+
